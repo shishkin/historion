78ae614d679c5f001d1c4ef6fe3c5f8afa62ed0b
Sergey Shishkin <sergei.shishkin@gmail.com>
2015-09-18 15:54:03 +0800

    add file summary

--- core/src/main/scala/historion/Analyses.scala
+++ core/src/main/scala/historion/Analyses.scala
@@ -11,5 +11,17 @@ object Analyses {
       totalAuthors = changes.map(_._1.author).distinct.length,
       totalFiles = changes.map(_._2.path).distinct.length,
       fileChanges = changes.length)
+
+    def fileSummary(): GenSeq[(String, Summary)] = changes
+      .map { case (commit, change) => (change.path, commit.author) }
+      .groupBy(_._1)
+      .mapValues(
+        changes => Summary(
+          totalCommits = changes.length,
+          totalAuthors = changes.map(_._2).distinct.length,
+          totalFiles = 1,
+          fileChanges = changes.length))
+      .toIndexedSeq
+      .sortBy(-_._2.totalCommits)
   }
 }
